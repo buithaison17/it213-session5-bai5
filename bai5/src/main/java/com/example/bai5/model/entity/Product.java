656d359647af82e0d75c479eb4afa2b6be4a9525
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2026-08-19 08:23:59 +0700

    Upload bai5/src/main/java/com/example/bai5/model/entity/Product.java

---
+++ bai5/src/main/java/com/example/bai5/model/entity/Product.java
@@ -0,0 +1,22 @@
+package com.example.bai5.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "products")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String description;
+    private double price;
+}
