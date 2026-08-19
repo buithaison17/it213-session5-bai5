bcb0161b8beb9eb8fea9ed1997d63d35f4cae84f
buithaison17 <125809724+buithaison17@users.noreply.github.com>
2026-08-19 08:23:54 +0700

    Upload bai5/src/main/java/com/example/bai5/repository/ProductRepository.java

---
+++ bai5/src/main/java/com/example/bai5/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.example.bai5.repository;
+
+import com.example.bai5.model.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
